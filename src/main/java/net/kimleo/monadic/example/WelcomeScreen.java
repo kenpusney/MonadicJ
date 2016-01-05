ff44db24eeac0f9044a1e766600228666560882f
KimmyLeo <kenpusney@gmail.com>
2016-01-05 13:14:16 +0800

    [Kimmy Leo] Rename nameOf to hello

--- src/main/java/net/kimleo/monadic/example/WelcomeScreen.java
+++ src/main/java/net/kimleo/monadic/example/WelcomeScreen.java
@@ -6,10 +6,10 @@ import net.kimleo.monadic.optional.Optional;
 public class WelcomeScreen {
 
     public String welcome(Customer customer) {
-        return nameOf(customer).value();
+        return hello(customer).value();
     }
 
-    private Optional<String> nameOf(Customer customer) {
+    private Optional<String> hello(Customer customer) {
         return customer.getNickName()
                 .or(customer.getName())
                 .map(name -> String.format("Hello %s", name));
