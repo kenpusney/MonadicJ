48cdc22cc4b6665b626499cb94b95fd0138a1454
KimmyLeo <kenpusney@gmail.com>
2016-01-05 17:58:07 +0800

    [Kimmy Leo] Extract method

--- src/main/java/net/kimleo/monadic/example/WelcomeScreen.java
+++ src/main/java/net/kimleo/monadic/example/WelcomeScreen.java
@@ -5,15 +5,23 @@ import net.kimleo.monadic.either.Either;
 public class WelcomeScreen {
 
     public Either<String, String> welcome(Customer customer) {
-        return Either.right(customer.getNickName()
-                        .or(customer.getName()).value())
+        return validate(nameOf(customer))
+                .bind(name -> Either.right(String.format("Hello %s", name)));
+    }
+
+    private Either<Object, String> validate(Either<Object, String> nameOfCustomer) {
+        return nameOfCustomer
                 .where("Customer name length must between 4 and 18",
                         name -> name.length() < 19 && name.length() >= 4)
                 .where("Customer name must start with letters",
                         name -> Character.isAlphabetic(name.charAt(0)))
                 .where("Customer name should not contain special characters",
-                        name -> name.matches("^(\\w|\\d|\\s)+$"))
-                .bind(name -> Either.right(String.format("Hello %s", name)));
+                        name -> name.matches("^(\\w|\\d|\\s)+$"));
+    }
+
+    private Either<Object, String> nameOf(Customer customer) {
+        return Either.right(customer.getNickName()
+                .or(customer.getName()).value());
     }
 
 }
